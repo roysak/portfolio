c7d2a1582baadcdb0ee126f948f8a6f359bdd0b0
Roys A Kareem <Roys.AbdulKareem@ust.com>
2026-04-18 15:14:40 +0530

    changes

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import Cursor from "../components/Cursor";
 
 export default function RootLayout() {
   return (
     <div className="grid h-screen grid-rows-[min-content_auto_min-content] bg-white text-neutral-900 font-sans antialiased">
-      {/* <Cursor /> */}
       <Nav />
       <div>
         <Outlet />
