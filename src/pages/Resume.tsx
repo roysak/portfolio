a0c5034030925fb117f438c3c21e682ee5586c1b
Roys A Kareem <roysak@gmail.com>
2026-04-19 14:34:53 +0530

    removed unused imports

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom";
 
 export default function Resume() {
   return (
     <main className="max-w-6xl w-full mx-auto px-6 py-24 pt-12">
-      <h1 className="text-3xl font-semibold tracking-tight mb-10">Resume</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <h1 className="text-4xl md:text-5xl font-semibold tracking-tight mb-4">
             Roys A Kareem
