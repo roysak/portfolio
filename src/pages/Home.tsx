4057c0cf73ee2e99486b6106943d4cd2de13b5a3
Roys A Kareem <roysak@gmail.com>
2026-04-19 23:01:39 +0530

    spacing corrections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,11 @@ export default function Home() {
                 <div className="w-full mx-auto md:px-24 flex flex-col lg:flex-row gap-16 md:gap-32 items-center justify-center">
                     <div className="flex flex-col xl:flex-row gap-0 items-center">
                         <div className="flex flex-col lg:flex-row gap-0 items-center">
-                            <div className="pointer-events-none items-center justify-center opacity-80 hidden lg:flex min-w-sm">
-                                <img src={assetUrl("/img/dude.png")} alt="A portrait of the developer" className="w-full max-w-md lg:max-w-md" />
+                            <div className="pointer-events-none items-center justify-center opacity-80 lg:flex min-w-sm">
+                                <img src={assetUrl("/img/dude.png")} alt="A portrait of the developer" className="w-full max-w-50 md:max-w-xs m-auto" />
                             </div>
                             <div className="flex flex-col w-fll lg:w-auto gap-6 md:gap-6 items-stretch px-6 py-6">
-                                {/* <h1 className="text-8xl md:text-8xl lg:text-8xl tracking-tight mb-4 text-primary-600 w-full sm:text-center lg:text-left"> */}
-                                <h1 className="text-8xl md:text-8xl lg:text-8xl tracking-tight mb-4 text-primary-600 w-full text-center lg:text-left">
+                                <h1 className="text-6xl xl:text-8xl tracking-tight mb-4 text-primary-600 w-full text-center lg:text-left md:pt-8">
                                     Pixels. Code.
                                     <div className="font-semibold">
                                         <span className="text-[#df5745]">I</span>
@@ -32,27 +31,24 @@ export default function Home() {
                         <div className="flex flex-col px-6 pt-16 sm:w-full md:w-auto sm:flex-col md:flex-row lg:pt-0 xl:flex-col gap-6 border-neutral-200 lg:border-l lg:px-12">
                             <Link
                                 to="/case-studies"
-                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
+                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
                                 <span className="material-symbols-rounded text-primary-600 shrink-0 !text-4xl group-hover/item:text-white" aria-hidden="true">book</span>
                                 <b className="font-semibold text-2xl">Case Studies</b>
                                 <p className="text-lg">Deep dives into the "why" behind the "what."</p>
-                                 {/* Explore the strategic thinking, user research, and iterative processes that transformed complex problems into functional solutions. */}
                             </Link>
                             <Link
                                 to="/works"
-                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
+                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
                                 <span className="material-symbols-rounded text-primary-600 shrink-0 !text-4xl group-hover/item:text-white" aria-hidden="true">work</span>
                                 <b className="font-semibold text-2xl">Works</b>
                                 <p className="text-lg">A curated gallery of craft and code.</p>
-                                {/* A showcase of live projects and digital experiences built at the intersection of aesthetic design and technical precision. */}
                             </Link>
                             <Link
                                 to="/resume"
-                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
+                                className="group/item inline-flex flex-col items-start gap-2 px-8 py-4 border border-neutral-300 text-neutral-700 text-lg rounded-xl hover:border-neutral-500 transition-all hover:bg-primary-600 hover:text-white">
                                 <span className="material-symbols-rounded text-primary-600 shrink-0 !text-4xl group-hover/item:text-white" aria-hidden="true">article_person</span>
                                 <b className="font-semibold text-2xl">Resume</b>
                                 <p className="text-lg">The journey, the tools, and the milestones.</p>
-                                {/* A detailed look at my professional evolution, technical stack, and the impact I’ve delivered across various industries. */}
                             </Link>
                         </div>
                     </div>
